869b3123a3fdc07c2ac988c45c3eb438b6f871c0
Steven Sucy <sms6462@g.rit.edu>
2015-03-12 01:27:06 -0400

    Fixed checking if fast draw is supported

--- src/com/rit/sucy/gui/MapBuffer.java
+++ src/com/rit/sucy/gui/MapBuffer.java
@@ -8,7 +8,16 @@ import java.lang.reflect.Method;
 
 /**
  * A specific MapImage used to represent a full map canvas
- * and draw to it as efficiently as possible
+ * and draw to it as efficiently as possible.
+ *
+ * Just as a comparison between render methods:
+ *   - Built in MapCanvas.drawImage: ~40ms for 128x128 image
+ *   - Slow MapBuffer method: ~ 0.08ms for 128x128 image
+ *   - Fast MapBuffer method: ~ 0.001ms for 128x128 image
+ *
+ * In other words:
+ *   - Slow MapBuffer method is ~500x faster than MapCanvas.drawImage
+ *   - Fast MapBuffer method is ~40,000x faster than MapCanvas.drawImage
  */
 public class MapBuffer extends MapImage
 {
@@ -25,10 +34,32 @@ public class MapBuffer extends MapImage
     {
         super(128, 128);
 
+        // Grab the needed values for fast rendering via reflection
         worldMap = Reflection.getValue(view, "worldMap");
         flagDirty = Reflection.getMethod(worldMap, "flagDirty", int.class, int.class);
+
+        // Older servers use a slightly different method
         if (flagDirty == null) {
             flagDirty2 = Reflection.getMethod(worldMap, "flagDirty", int.class, int.class, int.class);
+        }
+
+        // Just validate the methods before assuming they're good to go
+        try
+        {
+            if (flagDirty != null)
+            {
+                flagDirty.invoke(worldMap, 0, 0);
+            }
+            if (flagDirty2 != null)
+            {
+                flagDirty2.invoke(worldMap, 0, 0);
+            }
+        }
+        catch (Exception ex)
+        {
+            // Test failed, they aren't supported after all
+            flagDirty = null;
+            flagDirty2 = null;
         }
     }
 
@@ -41,7 +72,7 @@ public class MapBuffer extends MapImage
      */
     public boolean fastDrawSupported()
     {
-        return flagDirty != null;
+        return flagDirty != null || flagDirty2 != null;
     }
 
     /**
@@ -57,8 +88,11 @@ public class MapBuffer extends MapImage
         {
             try
             {
+                // Flag the min/max bounds
                 flagDirty.invoke(worldMap, 0, 0);
                 flagDirty.invoke(worldMap, 127, 127);
+
+                // Apply the buffer data
                 Reflection.setValue(canvas, "buffer", getData());
                 fast = true;
             }
@@ -74,8 +108,11 @@ public class MapBuffer extends MapImage
         {
             try
             {
+                // Flag the min/max bounds
                 flagDirty2.invoke(worldMap, 0, 0, 127);
                 flagDirty2.invoke(worldMap, 1, 0, 127);
+
+                // Apply the buffer data
                 Reflection.setValue(canvas, "buffer", getData());
                 fast = true;
             }
@@ -89,6 +126,9 @@ public class MapBuffer extends MapImage
         // Otherwise use the tried and true method
         if (!fast)
         {
+            // Just set each pixel individually
+            // Not terrible since images are pre-processed
+            // (about 500 times faster), but still not great.
             byte[] data = getData();
             for (int i = 0; i < data.length; i++)
             {
